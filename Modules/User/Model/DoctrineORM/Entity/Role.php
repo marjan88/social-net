ca0ac9f7aa856969190258d06ef2eb28e9db55c6
Aleksandar <petresevic@gmail.com>
2016-01-21 13:58:33 +0100

    adding doctrine auth

---
+++ Modules/User/Model/DoctrineORM/Entity/Role.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\User\Model\DoctrineORM\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use LaravelDoctrine\ORM\Contracts\Role as RoleContract;
+
+class Role implements RoleContract
+{
+    
+}
--- Modules/User/Providers/UserServiceProvider.php
+++ Modules/User/Providers/UserServiceProvider.php
@@ -13,7 +13,6 @@ class UserServiceProvider extends ServiceProvider
     protected $middleware = [
         'User' => [
             'cors' => 'Cors',
-            
         ],
     ];
 
@@ -25,13 +24,16 @@ class UserServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerMiddleware($this->app['router']);
-        
+
         $this->app->make('Modules\User\Http\Controllers\UserController');
         $this->loadViewsFrom(__DIR__ . '/../Views', 'user');
 
         $this->publishes([
             __DIR__ . '/../Views' => base_path('resources/views/vendor/user'),
+            __DIR__ . '/../config/module.auth.php' => config_path('module.auth.php'),
         ]);
+        $this->app['config']['auth'] =  \Config::get('module.auth');
+       
     }
 
     /**
@@ -44,8 +46,9 @@ class UserServiceProvider extends ServiceProvider
         $this->app->bind(
                 'Illuminate\Contracts\Auth\Registrar', 'Modules\User\Services\Registrar'
         );
+       
     }
-    
+
     private function registerMiddleware($router)
     {
         foreach ($this->middleware as $module => $middlewares) {
